0543f2544353d4cc4d1c0710b95934b6d2b587a6
ZhaoKaiYang123 <2563887180@qq.com>
2023-03-07 11:18:10 +0800

    Add project

---
+++ risk_judeg_java/src/main/java/com/example/risk_judeg_java/RiskJudegJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.risk_judeg_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskJudegJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskJudegJavaApplication.class, args);
+    }
+
+}
---
+++ risk_judeg_java/src/test/java/com/example/risk_judeg_java/RiskJudegJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.risk_judeg_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskJudegJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
