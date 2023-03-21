2707cf9ec8a92f83623d8442a7a00814cacce7de
ZhaoKaiYang123 <2563887180@qq.com>
2023-03-21 13:34:33 +0800

    修改项目名称

---
+++ risk_judge_java/src/test/java/com/example/risk_judge_java/RiskJudegJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.risk_judge_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskJudegJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
